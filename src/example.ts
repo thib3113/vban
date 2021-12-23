e19be2ca875389b5ee400f893fe7af610ce03e26
thibaut severac <thib3113@gmail.com>
2021-12-23 17:56:19 +0100

    update example

--- src/example.ts
+++ src/example.ts
@@ -1,96 +1,109 @@
 import dgram from 'dgram';
-import Speaker from 'speaker';
 import { VBANProtocol } from './VBANProtocol';
-import { VBANAudioPacket } from './packets/VBANAudioPacket/VBANAudioPacket';
-import { VBANServicePacket } from './packets/VBANServicePacket/VBANServicePacket';
-import { VBANSerialPacket } from './packets/VBANSerialPacket/VBANSerialPacket';
-import { EFormatBit } from './commons';
-import { VBANPacket } from './packets';
-import { ESerialStreamType } from './packets/VBANSerialPacket/ESerialStreamType';
-
-function hex2bin(hex: string) {
-    return parseInt(hex, 16).toString(2).padStart(8, '0');
-}
+import { EFormatBit, sampleRates } from './commons';
+import {
+    ESerialStreamType,
+    EServiceFunction,
+    EServicePINGApplicationType,
+    EServicePINGFeatures,
+    EServiceType,
+    ETextStreamType,
+    VBANAudioPacket,
+    VBANSerialPacket,
+    VBANServicePacket,
+    VBANTEXTPacket
+} from './packets';
+import * as os from 'os';
+import { Buffer } from 'buffer';
 
 const server = dgram.createSocket('udp4');
-
-let currentConfig: ISpeakerConfiguration;
-
-let speaker: Speaker;
-
-interface ISpeakerConfiguration extends Record<string, unknown> {
-    channels: number;
-    bitDepth: number;
-    sampleRate: number;
-    signed: boolean;
-    float: boolean;
-    samplesPerFrame: number;
-}
 
 server.on('error', (err) => {
     console.log(`server error:\n${err.stack}`);
     server.close();
 });
 
-server.on('message', (msg, rinfo) => {
+server.on('message', (msg, sender) => {
     try {
         const packet = VBANProtocol.processPacket(msg);
-        // console.log('data', data);
-
-        // const { header } = packet;
-        // console.log(
-        //     'headers',
-        //     `sample rates : ${header.sr}, subProtocol : ${ESubProtocol[header.sp]}, nbSample : ${header.nbSample}, nbChannel : ${
-        //         header.nbChannel
-        //     }, bitResolution : ${EBitsResolutions[header.bitResolution]} ${JSON.stringify(bitResolutions[header.bitResolution])}, codec : ${
-        //         header.codec
-        //     }, streamName : ${header.streamName}, frameCounter : ${header.frameCounter}`
-        // );
 
         if (packet instanceof VBANAudioPacket) {
-            const newConfig = headerToSpeakerConfig(packet);
-
-            VBANPacket.checkFrameCounter(packet);
-
-            let configMatch = true;
-            Object.keys(currentConfig || {}).forEach((element) => {
-                configMatch = configMatch && currentConfig[element] == newConfig[element];
-            });
-
-            if (!speaker || !configMatch) {
-                speaker = new Speaker({ ...newConfig });
-                currentConfig = newConfig;
-            }
-            // Check for audio frame
-            // console.log(`receive audio : counter ${packet.frameCounter}, frame size : ${packet.dataBuffer.length}`);
-            speaker.write(packet.data);
+            // //generate speaker configuration from VBAN packet
+            // const newConfig = headerToSpeakerConfig(packet);
+            //
+            // let configMatch = true;
+            // Object.keys(currentConfig || {}).forEach((element) => {
+            //     configMatch = configMatch && currentConfig[element] == newConfig[element];
+            // });
+            //
+            // if (!speaker || !configMatch) {
+            //     speaker = new Speaker({ ...newConfig });
+            //     currentConfig = newConfig;
+            // }
+            //
+            // speaker.write(packet.data);
         }
         //check seriql packet
         else if (packet instanceof VBANSerialPacket) {
-            console.log(
-                `${packet.streamName}, format : ${EFormatBit[packet.formatBit as number]}, stream type : ${
-                    ESerialStreamType[packet.streamType as number]
-                }`
+            const packet1 = new VBANSerialPacket(
+                {
+                    bitMode: { stop: 1, start: false, parity: false, multipart: false },
+                    bps: 0,
+                    channelsIdents: 0,
+                    formatBit: EFormatBit.VBAN_DATATYPE_BYTE8,
+                    frameCounter: 0,
+                    streamName: '',
+                    streamType: ESerialStreamType.VBAN_SERIAL_MIDI,
+                    sr: sampleRates[4]
+                },
+                Buffer.from('b0036a', 'hex')
             );
-            console.log(packet);
         } else if (packet instanceof VBANServicePacket) {
-            console.log('base');
-            console.log('VBANPING_TYPE_RECEPTOR', 0x00000001);
-            console.log('VBANPING_TYPE_TRANSMITTER', 0x00000002);
-            console.log('VBANPING_TYPE_RECEPTORSPOT', 0x00000004);
-            console.log('VBANPING_TYPE_TRANSMITTERSPOT', 0x00000008);
-            console.log('VBANPING_TYPE_VIRTUALDEVICE', 0x00000010);
-            console.log('VBANPING_TYPE_VIRTUALMIXER', 0x00000020);
-            console.log('VBANPING_TYPE_MATRIX', 0x00000040);
-            console.log('VBANPING_TYPE_DAW', 0x00000080);
-            console.log('VBANPING_TYPE_SERVER', 0x01000000);
+            console.log(
+                `receive message from ${sender.address}:${sender.port} . Hostname : ${packet.data.reservedLongASCII}, Device ${packet.data.deviceName}, Application ${packet.data.applicationName}, Language ${packet.data.langCode}`,
+                JSON.stringify(packet)
+            );
 
-            console.log('end');
+            const newPacket = new VBANServicePacket(
+                {
+                    streamName: 'VBAN Service',
+                    service: EServiceType.IDENTIFICATION,
+                    serviceFunction: EServiceFunction.PING0,
+                    frameCounter: 1
+                },
+                {
+                    applicationName: 'VBAN Example',
+                    manufacturerName: 'Anonymous',
+                    applicationType: EServicePINGApplicationType.SERVER,
+                    features: [
+                        EServicePINGFeatures.AUDIO,
+                        EServicePINGFeatures.MIDI,
+                        EServicePINGFeatures.TXT,
+                        EServicePINGFeatures.SERIAL
+                    ],
+                    bitFeatureEx: 0,
+                    PreferredRate: 0,
+                    minRate: 6000,
+                    maxRate: 705600,
+                    color: { blue: 74, green: 232, red: 57 },
+                    nVersion: 12345,
+                    GPSPosition: '',
+                    userPosition: '',
+                    langCode: 'fr-fr',
+                    reservedASCII: '',
+                    reservedEx: '',
+                    reservedEx2: '',
+                    deviceName: 'NodeJs Server',
+                    reservedLongASCII: os.hostname(),
+                    userName: '',
+                    userComment: ''
+                }
+            );
+            //send the answer to sender IP:port . (VM use listen port to send requests)
+            server.send(VBANServicePacket.toUDPPacket(newPacket), sender.port, sender.address);
         } else {
             console.log('receive text : ', packet.text);
         }
-
-        // console.log(packet.frameCounter);
     } catch (e) {
         console.error(e);
     }
@@ -99,20 +112,17 @@ server.on('message', (msg, rinfo) => {
 server.on('listening', () => {
     const address = server.address();
     console.log(`server listening ${address.address}:${address.port}`);
+
+    const textPacket = new VBANTEXTPacket(
+        {
+            streamName: 'Command1',
+            frameCounter: 1,
+            streamType: ETextStreamType.VBAN_TXT_UTF8,
+            formatBit: EFormatBit.VBAN_DATATYPE_BYTE8
+        },
+        'test'
+    );
+    server.send(VBANTEXTPacket.toUDPPacket(textPacket), 7000, '192.168.1.2');
 });
 
 server.bind(7000);
-// Prints: server listening 0.0.0.0:41234
-
-function headerToSpeakerConfig(header: VBANAudioPacket): ISpeakerConfiguration {
-    const bitResolution = VBANAudioPacket.bitResolutions[header.bitResolution as number];
-
-    return {
-        channels: header.nbChannel,
-        bitDepth: bitResolution.bitDepth,
-        sampleRate: header.sr,
-        signed: bitResolution.signed,
-        float: bitResolution.float,
-        samplesPerFrame: header.nbSample
-    };
-}
