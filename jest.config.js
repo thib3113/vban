fc73083d21d889e3b10f705497c0b80429d4538c
thibaut severac <thib3113@gmail.com>
2022-01-31 17:25:47 +0100

    error in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
+const path = require('path');
 
 module.exports = {
     // All imported modules in your tests should be mocked automatically
